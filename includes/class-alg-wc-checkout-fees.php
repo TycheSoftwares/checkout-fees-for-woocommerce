002c45b14e66950a773a205d906418515496748a
nidhinikulpatel <tatosaniyanidhi@gmail.com>
2023-01-17 14:26:57 +0530

    Fix #119
    Fixed fatal error when max total fee and total discounts are empty.

--- includes/class-alg-wc-checkout-fees.php
+++ includes/class-alg-wc-checkout-fees.php
@@ -516,6 +516,10 @@ if ( ! class_exists( 'Alg_WC_Checkout_Fees' ) ) :
 				$new_fee = $max_fee;
 			}
 			// Max total discount.
+			if ( empty( $this->max_total_all_discounts ) || empty( $this->max_total_all_fees ) ) {
+				$this->max_total_all_discounts = 0;
+				$this->max_total_all_fees      = 0;
+			}
 			if ( false !== $this->max_total_all_discounts ) {
 				if ( $new_fee < $this->max_total_all_discounts ) {
 					$new_fee = $this->max_total_all_discounts;
