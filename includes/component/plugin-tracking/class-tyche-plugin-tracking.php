7863eb2498d0789eb7180283b3d14f3819aace6b
Nidhi <nidhi@tychesoftwares.com>
2024-03-20 10:17:04 +0530

    Fixed feedback changes

--- includes/component/plugin-tracking/class-tyche-plugin-tracking.php
+++ includes/component/plugin-tracking/class-tyche-plugin-tracking.php
@@ -147,15 +147,8 @@ if ( ! class_exists( 'Tyche_Plugin_Tracking' ) ) {
 		 * Called when the dismiss icon is clicked on the notice.
 		 */
 		public function dismiss_notice() {
-			if ( ! is_user_logged_in() ) {
-				return;
-			}
-			$current_user = wp_get_current_user();
-			if ( ! in_array( 'administrator', $current_user->roles, true ) || ! current_user_can( 'manage_options' ) ) {
-				return;
-			}
 			$nonce = $_POST['tracking_notice'];//phpcs:ignore
-			if ( ! wp_verify_nonce( $nonce, 'tracking_notice' ) ) {
+			if ( ! is_user_logged_in() || ! current_user_can( 'manage_options' ) || ! wp_verify_nonce( $nonce, 'tracking_notice' ) ) {
 				return;
 			}
 			update_option( $this->plugin_short_name . '_allow_tracking', 'dismissed' );
